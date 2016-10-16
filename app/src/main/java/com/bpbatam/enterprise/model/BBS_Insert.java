ca3e67d5411d015af1d2061d510f4fbc640259d4
seimase <nugraha.setia@gmail.com>
2016-10-17 06:19:20 +0700

    Add API BBS Insert

---
+++ app/src/main/java/com/bpbatam/enterprise/model/BBS_Insert.java
@@ -0,0 +1,63 @@
+package com.bpbatam.enterprise.model;
+
+import java.util.List;
+
+public class BBS_Insert {
+    public String code ;
+    public String info ;
+
+    public  String hashid;
+    public  String userid;
+    public String reqid;
+    public String title;
+    public String name;
+    public String start_period;
+    public String content;
+    public String bbs_date;
+    public String priority_id;
+    public String read;
+    public String category_id;
+    public String create_by;
+    public String reply_id;
+    public String create_time;
+    /*      "title": "forum123",
+            "name": "iwan",
+            "start_period": "2016-09-09",
+            "end_period": "2016-09-12",
+            "content": "content bbs contoh2",
+            "bbs_date": "2016-09-20",
+            "priority_id": "1",
+            "read": "0",
+            "category_id": "FRU",
+            "create_by":"user2",
+            "reply_id":"4",
+            "create_time":"2016-09-02 20:09:09"
+*/
+    public BBS_Insert(String hashid, String userid, String reqid, String title,
+                      String name,
+                      String start_period,
+                      String content,
+                      String bbs_date,
+                      String priority_id,
+                      String read,
+                      String category_id,
+                      String create_by,
+                      String reply_id,
+                      String create_time) {
+        this.hashid = hashid;
+        this.userid = userid;
+        this.reqid = reqid;
+        this.read = read;
+        this.name = name;
+        this.title = title;
+        this.start_period = start_period;
+        this.content = content;
+        this.bbs_date = bbs_date;
+        this.priority_id = priority_id;
+        this.category_id = category_id;
+        this.create_by = create_by;
+        this.reply_id = reply_id;
+        this.create_time = create_time;
+    }
+
+}
